0378562acd8d37baac5fd2518033c330a1447c2e
Chanda Hubbard <chanda.hubbard@gmail.com>
2020-04-08 17:37:20 -0700

    added movie dataset

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const helmet = require("helmut");
 const cors = require("cors");
-const MOVIEDEX = require("./moviedex.json");
+const helmet = require("helmut");
+const MOVIES = require("./moviedex.json");
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(helmut());
 app.use(cors());
+app.use(helmut());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -18,39 +18,37 @@ app.use(function validateBearerToken(req, res, next) {
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
-
   next();
 });
 
 app.get("/movie", function handleGetMovie(req, res) {
-  let response = MOVIEDEX.film_title;
+  let response = MOVIES;
 
   if (req.query.genre) {
-    response = response.filter((film_title) =>
-      film_title.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    );
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
   }
 
   if (req.query.country) {
-    response = response.filter((film_title) =>
-      film_title.country.toLowerCase().includes(req.query.country.toLowerCase())
-    );
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
   }
 
   if (req.query.avg_vote) {
-    //When searching by average vote, 
-    //users are searching for Movies with an 
-    //avg_vote that is greater than or equal to 
+    //When searching by average vote,
+    //users are searching for Movies with an
+    //avg_vote that is greater than or equal to
     //the supplied number.
-    response = response.filter((film_title) =>
-      film_title.avg_vote
-        .toLowerCase()
-        .includes(req.query.avg_vote.toLowerCase())
-    );
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
   }
+  res.json(response)
 });
 
-const PORT = 8002;
+const PORT = 9000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
